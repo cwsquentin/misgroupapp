bced106ab2a13dd2ea6b8d95185b55f0d3663fa9
Quentin <quentin.petiteville@student.junia.com>
2025-11-30 16:51:48 +0100

    fix: correct spelling

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -10,7 +10,7 @@ export default function AboutScreen() {
         A propos
       </ThemedText>
       <ThemedText style={styles.body}>
-        Cette application a été réalisée par Quentin Petiteville pour MIS Groupe dans le cadre d&apos;un test
+        Cette application a été réalisée par Quentin Petiteville pour MIS Group dans le cadre d&apos;un test
         technique. Elle permet d&apos;ajouter rapidement des prénoms, de les garder sous la main et de les relire
         facilement.
       </ThemedText>
